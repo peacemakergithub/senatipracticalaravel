0b29f334a29059869d8b9a269934737c88bec9e2
pacificadorgithub <alche_2341@hotmail.com>
2023-10-11 23:21:45 -0500

    cambios 03

--- database/migrations/2023_10_10_71571704_create_cursos_table.php
+++ database/migrations/2023_10_10_71571704_create_cursos_table.php
@@ -12,16 +12,15 @@ return new class extends Migration
     public function up():void
     {
         Schema::create('cursos', function (Blueprint $table) {
-            $table->id(); // Columna de ID autoincremental, común para la mayoría de las tablas.
-            $table->string('nombre',50)->unique(); // Nombre del curso, columna de tipo string.
-            $table->text('descripcion'); // Descripción del curso, columna de tipo texto.
-            $table->integer('duracion_horas'); // Duración del curso en horas, columna de tipo entero.
-            $table->date('fecha_inicio'); // Fecha de inicio del curso, columna de tipo fecha.
-            $table->enum('nivel', ['principiante', 'intermedio', 'avanzado']); // Nivel del curso, columna de tipo enumeración.
-            $table->decimal('precio', 10, 2); // Precio del curso, columna de tipo decimal con 10 dígitos en total y 2 decimales.
-            // Agrega otras columnas según tus necesidades.
-            $table->enum('idioma', ['español', 'español e inglés', 'inglés','otros']); // Agregamos la columna "idioma".
-            $table->timestamps(); // Columnas de fecha de creación y actualización automática.
+            $table->id(); 
+            $table->string('nombre',50)->unique(); 
+            $table->text('descripcion'); 
+            $table->integer('duracion_horas'); 
+            $table->date('fecha_inicio'); 
+            $table->enum('nivel', ['principiante', 'intermedio', 'avanzado']); 
+            $table->decimal('precio', 10, 2); 
+            $table->enum('idioma', ['español', 'español e inglés', 'inglés','otros']); 
+            $table->timestamps(); 
         });
     }
 
